f0ab2202161dd8e432d5c954083f07c4a83c97ee
Charles Stein <charles.mst@gmail.com>
2019-11-12 23:32:26 -0300

    contact crud state tests

--- src/components/PeopleState.js
+++ src/components/PeopleState.js
@@ -17,10 +17,34 @@ export function reducer(state, action) {
         case 'SAVE_PERSON_SUCCESS_DELETE':
             return { ...state, loadingSave: false, data: state.data.filter(x => x.id != action.id), errorSave: null }
 
+
+
+        case 'SAVE_CONTACT_SUCCESS_ADD':
+            return {
+                ...state, loadingSave: false, data: state.data.map(x => x.id === action.personId ? {
+                    ...x, contacts: [
+                        ...(x.contacts || []),
+                        action.contact
+                    ],
+                } : x), errorSave: null
+            }
+        case 'SAVE_CONTACT_SUCCESS_UPDATE':
+            return {
+                ...state, loadingSave: false, data: state.data.map(x => x.id === action.personId ? {
+                    ...x, contacts: (x.contacts || []).map(y => y.id === action.contact.id ? { ...y, ...action.contact } : y),
+                } : x), errorSave: null
+            }
+        case 'SAVE_CONTACT_SUCCESS_DELETE':
+            return {
+                ...state, loadingSave: false, data: state.data.map(x => x.id === action.personId ? {
+                    ...x, contacts: (x.contacts || []).filter(y => y.id !== action.contactId),
+                } : x), errorSave: null
+            }
         case 'SAVE_PERSON_ERROR':
             return { ...state, loadingSave: false, errorSave: action.error }
 
     }
+    return state
 }
 export async function loadPeople(dispatch) {
     dispatch({ type: 'FETCH_PEOPLE_START' })
@@ -37,7 +61,7 @@ export async function loadPeople(dispatch) {
 export async function addPerson(dispatch, data) {
     dispatch({ type: 'SAVE_PERSON_START' })
     try {
-        const response = await axios.post('/person')
+        const response = await axios.post('/person', data)
         const id = response.data.id
         dispatch({ type: 'SAVE_PERSON_SUCCESS_ADD', person: { ...data, id } })
     } catch (e) {
@@ -50,7 +74,7 @@ export async function addPerson(dispatch, data) {
 export async function updatePerson(dispatch, id, data) {
     dispatch({ type: 'SAVE_PERSON_START' })
     try {
-        const response = await axios.put('/person/' + id)
+        const response = await axios.put('/person/' + id, data)
         dispatch({ type: 'SAVE_PERSON_SUCCESS_UPDATE', person: { ...data, id } })
     } catch (e) {
         dispatch({ type: 'SAVE_PERSON_ERROR', error: filterApiErrorMessage(e) })
@@ -63,6 +87,43 @@ export async function deletePerson(dispatch, id) {
     try {
         const response = await axios.delete('/person/' + id)
         dispatch({ type: 'SAVE_PERSON_SUCCESS_DELETE', id })
+    } catch (e) {
+        dispatch({ type: 'SAVE_PERSON_ERROR', error: filterApiErrorMessage(e) })
+    }
+
+}
+
+
+
+export async function addContact(dispatch, personId, data) {
+    dispatch({ type: 'SAVE_PERSON_START' })
+    try {
+        const response = await axios.post(`/contact/${personId}`, data)
+        const id = response.data.id
+        dispatch({ type: 'SAVE_CONTACT_SUCCESS_ADD', personId, contact: { ...data, id } })
+    } catch (e) {
+        dispatch({ type: 'SAVE_PERSON_ERROR', error: filterApiErrorMessage(e) })
+    }
+
+}
+
+
+export async function updateContact(dispatch, personId, id, data) {
+    dispatch({ type: 'SAVE_PERSON_START' })
+    try {
+        const response = await axios.put(`/contact/${personId}/${id}`)
+        dispatch({ type: 'SAVE_CONTACT_SUCCESS_UPDATE', personId, contact: { ...data, id } })
+    } catch (e) {
+        dispatch({ type: 'SAVE_PERSON_ERROR', error: filterApiErrorMessage(e) })
+    }
+
+}
+
+export async function deleteContact(dispatch, personId, id) {
+    dispatch({ type: 'SAVE_PERSON_START' })
+    try {
+        const response = await axios.delete(`/contact/${personId}/${id}` + id)
+        dispatch({ type: 'SAVE_CONTACT_SUCCESS_DELETE', personId, id })
     } catch (e) {
         dispatch({ type: 'SAVE_PERSON_ERROR', error: filterApiErrorMessage(e) })
     }
--- src/components/PeopleState.test.js
+++ src/components/PeopleState.test.js
@@ -1,10 +1,14 @@
 
 import Axios, { filterApiErrorMessage } from '../Axios'
 jest.mock('../Axios')
-import { reducer, loadPeople, addPerson, updatePerson, deletePerson } from "./PeopleState";
+import { reducer, loadPeople, addPerson, updatePerson, deletePerson, addContact, updateContact, deleteContact } from "./PeopleState";
 const fakePeopleData = [
     {
-        name: "Charles"
+        id: "id1",
+        name: "Charles",
+        contacts: [
+            { id: "contact2", type: 0, value: "55999999999" }
+        ]
     }
 ]
 it('should start loading when fetch starts', () => {
@@ -168,6 +172,75 @@ it('should delete person to api', async (done) => {
     expect(deleteMock).toHaveBeenCalledTimes(1)
     expect(dispatch).toHaveBeenNthCalledWith(1, { type: 'SAVE_PERSON_START' })
     expect(dispatch).toHaveBeenNthCalledWith(2, { type: 'SAVE_PERSON_SUCCESS_DELETE', id })
+    expect(dispatch).toHaveBeenCalledTimes(2)
+    done()
+})
+
+
+
+it('should add contact to person to list when save is succeess', () => {
+    const currentState = { loadingSave: true, data: fakePeopleData }
+    const result = reducer(currentState, { type: 'SAVE_CONTACT_SUCCESS_ADD', personId: fakePeopleData[0].id, contact: { id: "contact1" } })
+    expect(result.data).toEqual([{ ...fakePeopleData[0], contacts: [...fakePeopleData[0].contacts, { id: "contact1" }] }])
+})
+
+
+it('should update contact to person to list when update is succeess', () => {
+    const currentState = { loadingSave: true, data: fakePeopleData }
+    const newContactValue = { id: "contact2", type: 2, value: "newValue" }
+    const result = reducer(currentState, { type: 'SAVE_CONTACT_SUCCESS_UPDATE', personId: fakePeopleData[0].id, contact: newContactValue })
+    expect(result.data).toEqual([{ ...fakePeopleData[0], contacts: [newContactValue] }])
+})
+
+
+it('should remove contact from person  when delete  is succeess', () => {
+    const currentState = { loadingSave: true, data: fakePeopleData }
+    const result = reducer(currentState, { type: 'SAVE_CONTACT_SUCCESS_DELETE', personId: fakePeopleData[0].id, contactId: "contact2" })
+    expect(result.data).toEqual([{ ...fakePeopleData[0], contacts: [] }])
+})
+
+
+
+it('should add contact to api', async (done) => {
+    const postMock = jest.fn().mockReturnValueOnce(Promise.resolve({ data: { success: true, id: "id1" } }))
+    Axios.post.mockImplementationOnce(postMock)
+    const dispatch = jest.fn()
+    const entityData = { type: 0, value: "charles.mst@gmail.com" }
+    const personId = "person1"
+    await addContact(dispatch, personId, entityData)
+    expect(postMock).toHaveBeenCalledTimes(1)
+    expect(dispatch).toHaveBeenNthCalledWith(1, { type: 'SAVE_PERSON_START' })
+    expect(dispatch).toHaveBeenNthCalledWith(2, { type: 'SAVE_CONTACT_SUCCESS_ADD', personId, contact: { ...entityData, id: "id1" } })
+    expect(dispatch).toHaveBeenCalledTimes(2)
+    done()
+})
+
+
+it('should update contact to api', async (done) => {
+    const putMock = jest.fn().mockReturnValueOnce(Promise.resolve({ data: { success: true, id: "id1" } }))
+    Axios.put.mockImplementationOnce(putMock)
+    const dispatch = jest.fn()
+    const entityData = { type: 0, value: "charles.mst@gmail.com" }
+    const personId = "person1"
+    await updateContact(dispatch, personId, "id1", entityData)
+    expect(putMock).toHaveBeenCalledTimes(1)
+    expect(dispatch).toHaveBeenNthCalledWith(1, { type: 'SAVE_PERSON_START' })
+    expect(dispatch).toHaveBeenNthCalledWith(2, { type: 'SAVE_CONTACT_SUCCESS_UPDATE', personId, contact: { ...entityData, id: "id1" } })
+    expect(dispatch).toHaveBeenCalledTimes(2)
+    done()
+})
+
+
+it('should delete contact to api', async (done) => {
+    const deleteMock = jest.fn().mockReturnValueOnce(Promise.resolve({ data: { success: true, id: "id1" } }))
+    Axios.delete.mockImplementationOnce(deleteMock)
+    const dispatch = jest.fn()
+    const personId = "person1"
+    const id = "id1"
+    await deleteContact(dispatch, personId, id)
+    expect(deleteMock).toHaveBeenCalledTimes(1)
+    expect(dispatch).toHaveBeenNthCalledWith(1, { type: 'SAVE_PERSON_START' })
+    expect(dispatch).toHaveBeenNthCalledWith(2, { type: 'SAVE_CONTACT_SUCCESS_DELETE', personId, id })
     expect(dispatch).toHaveBeenCalledTimes(2)
     done()
 })
